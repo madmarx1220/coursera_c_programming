17d1029edf5c9ad24a730022862130a8cf610e7e
Coursera Learner <nobody@nowhere.nul>
2021-02-12 08:56:39 +0000

    rr

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -48,3 +48,18 @@ int main(int argc, char ** argv) {
   }
   return EXIT_SUCCESS;
 }
+
+
+void rotate(char matrix[10][10]) {
+  char tempMatrix[10][10];
+  for(int i = 0; i < 10; i++) {
+    for(int j = 0; j < 10; j++) {
+      tempMatrix[i][j] = matrix[9-j][i];
+    }
+  }
+  for(int i = 0; i < 10; i++) {
+    for(int j = 0; j < 10; j++) {
+      matrix[i][j] = tempMatrix[i][j];
+    }
+  }
+}
