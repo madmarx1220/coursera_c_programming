0a32b6073785658b4cdb2825f5b1bed968ea4689
Coursera Learner <nobody@nowhere.nul>
2021-04-24 06:50:05 +0000

    1

---
+++ c4prj2_input/future.c
@@ -0,0 +1,34 @@
+#include "future.h"
+
+void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
+  if(index < fc->n_decks) {
+    ((fc->decks[index]).n_cards)++;
+    (fc->decks[index]).cards = realloc((fc->decks[index]).cards, (fc->decks[index]).n_cards*sizeof(card_t*));
+    ((fc->decks[index]).cards)[(fc->decks[index]).n_cards - 1] = ptr;
+  }
+  else {
+    fc->decks = realloc(fc->decks, (index+1)*sizeof(*(fc->decks)));
+    for(size_t i=fc->n_decks; i<index; i++) {
+      (fc->decks[i]).n_cards = 0;
+      (fc->decks[i]).cards = NULL;
+    }
+    (fc->decks[index]).n_cards = 1;
+    ((fc->decks[index]).cards) = malloc(sizeof(card_t*));
+    *((fc->decks[index]).cards) = ptr;
+    fc->n_decks = index+1;
+  }
+}
+    
+void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
+  if(fc->n_decks > deck->n_cards) {
+    fprintf(stderr, "Not enough cards in the deck\n");
+  }
+  else {
+    for(size_t i=0; i<fc->n_decks; i++) {
+      for(size_t j=0; j<(fc->decks[i]).n_cards; j++) {
+	*(((fc->decks[i]).cards)[j]) = *(deck->cards[i]);
+      }
+    }
+  }
+}
+
---
+++ c4prj2_input/input.c
@@ -0,0 +1,54 @@
+#include "input.c"
+
+deck_t * hand_from_string(const char * str, future_cards_t *fc) {
+  deck_t * deck = malloc(sizeof(*deck));
+  deck->n_cards = 0;
+  deck->cards = NULL;
+  size_t unknown;
+  char value, suit;
+  card_t card;
+  char * ptr = str;
+  while(*ptr != '\0') {
+    if(*ptr == '?') {
+      ptr++;
+      unknown = *ptr - '0';
+      add_future_card(fc, unknown, add_empty_card(deck));
+      ptr++;
+    }
+    if(*ptr == ' ') {
+      ptr++;
+    }
+    else {
+      value = *ptr;
+      ptr++;
+      suit = *ptr;
+      ptr++;
+      card = card_from_letters(value, suit);
+      assert_card_valid(card);
+      add_card_to(deck, card);
+    }
+  }
+  if(deck->n_cards < 5) {
+    fprintf(stderr, "less than 5 cards per hand");
+    exit(EXIT_FAILURE);
+  }
+  return deck;
+}
+
+deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t *fc) {
+  char * line = NULL;
+  size_t sz = 0;
+  char * ptr;
+  deck_t ** decks = NULL;
+  *n_hands = 0;
+  while(getline(&line, &sz, f) >=0) {
+    ptr = strchr(line, '\n');
+    *ptr = '\0';
+    (*n_hands) += 1;
+    decks = realloc(decks, *n_hands * sizeof(*decks));
+    decks[*n_hands - 1] = hand_from_string(line, fc);
+  }
+  free(line);
+  return decks;
+}
+    
