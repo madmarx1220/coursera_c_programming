80f99ab30e7272d43fd4dcbfc37ecdf3c1c9240b
Coursera Learner <nobody@nowhere.nul>
2021-04-29 05:33:34 +0000

    c

--- c4prj2_input/main.c
+++ c4prj2_input/main.c
@@ -14,4 +14,18 @@ int main(int argc, char ** argv) {
     return EXIT_FAILURE;
   }
   size_t n_hands;
-  
+  future_cards_t * fc = malloc(sizeof(*fc));
+  fc->n_decks = 0;
+  fc->decks = NULL;
+  deck_t ** decks = read_input(f, &n_hands, fc);
+  deck_t * common_deck = build_remaining_deck(decks, n_hands);
+  future_cards_from_deck(common_deck, fc);
+  for(size_t i=0; i<n_decks; i++) {
+    print_hand(decks[i]);
+    printf("\n");
+    free_deck(decks[i]);
+  }
+  free(fc);
+  free(decks);
+  free_deck(common_deck);
+}
